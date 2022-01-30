eb4ce69859713bd03084e48f2b63d76fa94ca803
Albina <aalmukhametova@gmail.com>
2022-01-30 23:53:21 +0300

    fix auth

--- part2/authentication/serializers.py
+++ part2/authentication/serializers.py
@@ -7,3 +7,11 @@ class UserCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+    def create(self, validated_data):
+        user = User.objects.create(**validated_data)
+
+        user.set_password(validated_data["password"])
+        user.save()
+
+        return user
